cba78ae094bc2bf670742e3491fa1a6d18fafc07
cessarD <00103518@uca.edu.sv>
2019-11-03 15:06:13 -0600

    Updates

---
+++ precache-manifest.c81da91cae26302e9d7b60d7f96616ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a830be214ca2927836e8a0e8c41320",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "4e8782df21b81b92e1eb",
+    "url": "/react/static/css/main.cf2c5b1e.chunk.css"
+  },
+  {
+    "revision": "112268765d96c7be084c",
+    "url": "/react/static/js/2.8ad0b67c.chunk.js"
+  },
+  {
+    "revision": "4e8782df21b81b92e1eb",
+    "url": "/react/static/js/main.51098547.chunk.js"
+  },
+  {
+    "revision": "9230ed9b3262159a2c91",
+    "url": "/react/static/js/runtime-main.9b0c0744.js"
+  }
+]);
